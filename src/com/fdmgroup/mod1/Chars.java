d5ad1119d3f5a557ea9283bed8c267217208f7a6
essteer <essteer@pm.me>
2024-07-10 22:32:50 +0800

    chore: Complete module 1 exercises

---
+++ src/com/fdmgroup/mod1/Chars.java
@@ -0,0 +1,31 @@
+package com.fdmgroup.mod1;
+
+public class Chars {
+
+	public static void main(String[] args) {
+		
+		char letter = 'a';
+		int code = 98;
+		System.out.println(Integer.valueOf(letter));
+		System.out.println((int) letter);
+		System.out.println((char) code);
+		System.out.println((char) 36);
+		
+		System.out.println('c' - 'a');
+		System.out.println('z' - 'a');
+		
+		char char1 = 'x';
+		char char2 = 'y';
+		char char3 = 'z';
+		
+		String string1 = "z";
+		String string2 = "y";
+		
+		System.out.println(char1 + char2 + char3);
+		System.out.println(char1 + char2 + string2);
+		System.out.println(char1 + string1 + char3);
+		
+
+	}
+
+}
---
+++ src/com/fdmgroup/mod1/PrimitiveNumbers.java
@@ -0,0 +1,33 @@
+package com.fdmgroup.mod1;
+
+public class PrimitiveNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int favouriteNumber = 7;
+		System.out.println("My favourite number is " + favouriteNumber);
+		
+		int five = 5;
+		int three = 3;
+		System.out.println(five + three);
+		
+		int counter = 0;
+		counter ++;
+		counter += 5;
+		counter --;
+		counter -= 2;
+		counter *= 2;
+		System.out.println(counter);
+		
+		int a = 5;
+		int b = 2;
+		double c = 5;
+		double d = 2;
+		System.out.println(a / b);
+		System.out.println(c / d);
+		System.out.println(c / b);
+		System.out.println(a / d);
+
+	}
+
+}
