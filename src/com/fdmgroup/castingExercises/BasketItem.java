c1ca26086a19065502b5ab4b7dbe8611a6c3a237
essteer <essteer@pm.me>
2024-07-11 23:34:06 +0800

    chore: Complete casting exercises

---
+++ src/com/fdmgroup/castingExercises/BasketItem.java
@@ -0,0 +1,10 @@
+package com.fdmgroup.castingExercises;
+
+public interface BasketItem {
+	
+	public abstract String getName();
+	public abstract void setName(String name);
+	public abstract double getPrice();
+	public abstract void setPrice(double price);
+
+}
---
+++ src/com/fdmgroup/castingExercises/FoodItem.java
@@ -0,0 +1,8 @@
+package com.fdmgroup.castingExercises;
+
+public interface FoodItem {
+	
+	public abstract int getCalories();
+	public abstract void setCalories(int calories);
+
+}
