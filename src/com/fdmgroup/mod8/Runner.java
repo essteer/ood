bd6ba3bcd0ef7ecd0c45b02850d2d162e3b8052e
essteer <essteer@pm.me>
2024-07-11 10:28:22 +0800

    chore: Complete static keyword exercises

---
+++ src/com/fdmgroup/mod8/Runner.java
@@ -0,0 +1,24 @@
+package com.fdmgroup.mod8;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+		Employee.setMinimumWage(9.53);
+//		System.out.println(Employee.getMinimumWage());
+		
+		Employee employee1 = new Employee();
+		Employee employee2 = new Employee();
+		Employee employee3 = new Employee();
+		
+		employee1.setHourlyWage(1.99);
+		employee2.setHourlyWage(5.45);
+		employee3.setHourlyWage(10.55);
+		
+		System.out.println(employee1.getHourlyWage());
+		System.out.println(employee2.getHourlyWage());
+		System.out.println(employee3.getHourlyWage());
+
+	}
+
+}
---
+++ src/com/fdmgroup/staticKeywordExercises/Runner.java
@@ -0,0 +1,18 @@
+package com.fdmgroup.staticKeywordExercises;
+
+public class Runner {
+
+	public static void main(String[] args) {
+
+		double matter = 10;
+		double energy = 10;
+
+		double matterToEnergy = EnergyMatterCalculator.matterToEnergy(matter);
+		double energyToMatter = EnergyMatterCalculator.energyToMatter(energy);
+
+		System.out.println("Matter to energy: " + matterToEnergy);
+		System.out.println(matter * Math.pow(299792458, 2));
+		System.out.println("Energy to matter: " + energyToMatter);
+		System.out.println(energy / Math.pow(299792458, 2));
+	}
+}
