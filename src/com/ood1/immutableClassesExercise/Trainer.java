454fd3b8db3d9eb4744dd46c5875a8c3d3148d5a
essteer <essteer@pm.me>
2024-07-12 20:44:01 +0800

    feat: Complete immutable class exercises

---
+++ src/com/ood1/immutableClassesExercise/Trainer.java
@@ -0,0 +1,32 @@
+package com.ood1.immutableClassesExercise;
+
+import java.util.ArrayList;
+
+public class Trainer {
+	
+	private String name;
+	private ArrayList<String> streamsTaught;
+	
+	public Trainer(String name) {
+		this.name = name;
+		this.streamsTaught = new ArrayList<>();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void addStream(String stream) {
+		this.streamsTaught.add(stream);
+	}
+	
+	public ArrayList<String> getAllStreams() {
+		return this.streamsTaught;
+	}
+	
+
+}
