5af3e9ca7c011aac74ff32f059a0d793ca7ea313
essteer <essteer@pm.me>
2024-07-16 10:56:37 +0800

    fix: Update bubble sort algo

--- src/com/ood1/mod5advanced/BubbleSort.java
+++ src/com/ood1/mod5advanced/BubbleSort.java
@@ -11,8 +11,18 @@ public class BubbleSort {
 			boolean swapped = false;
 
 			for (int i = 0; i < rawArray.length - 1; ++i) {
+				
+				if (
+						// A > 0 but B <= 0
+						newArray[i] > 0 && newArray[i+1] <= 0 ||
+						// A >= 0 but B < 0
+						newArray[i] >= 0 && newArray[i+1] < 0 ||
+						// A < 0 && B < 0 but A > B
+						(newArray[i] <= 0 && newArray[i+1] <= 0 && newArray[i] < newArray[i+1]) ||
+						// A > 0 && B > 0 but A > B
+						newArray[i] > newArray[i + 1]
+				) {
 
-				if (newArray[i] > newArray[i + 1]) {
 					int temp = newArray[i];
 					newArray[i] = newArray[i + 1];
 					newArray[i + 1] = temp;
