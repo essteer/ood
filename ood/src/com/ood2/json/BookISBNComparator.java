a779282e5c6b1372ebb2508b15ae50eba61cae00
essteer <essteer@pm.me>
2024-07-22 18:55:53 +0800

    feat: Complete JSON exercises

---
+++ ood/src/com/ood2/json/BookISBNComparator.java
@@ -0,0 +1,12 @@
+//package com.ood2.json;
+//
+//import java.util.Comparator;
+//
+//public class BookISBNComparator implements Comparator<Book> {
+//	
+//	@Override
+//	public int compare(Book bookA, Book bookB) {
+//		return bookA.getIsbn().compareTo(bookB.getIsbn());
+//	}
+//
+//}
