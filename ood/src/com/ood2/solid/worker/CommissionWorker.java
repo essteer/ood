a9bc2fd419548f13f758b0b8155f6a8bd12af467
essteer <essteer@pm.me>
2024-07-25 19:53:07 +0800

    feat: Add SOLID exercise solution

---
+++ ood/src/com/ood2/solid/worker/CommissionWorker.java
@@ -0,0 +1,20 @@
+package com.ood2.solid.worker;
+
+public class CommissionWorker extends SalariedWorker {
+	
+	private double commissionRate;
+	
+	public CommissionWorker(int id, String name, double salary, double rate) {
+		super(id, name, salary);
+		this.commissionRate = rate;
+	}
+	
+	public double getCommissionRate() {
+		return this.commissionRate;
+	}
+	
+	public void setCommissionRate(double newRate) {
+		this.commissionRate = newRate;
+	}
+	
+}
---
+++ ood/src/com/ood2/solid/worker/Worker.java
@@ -0,0 +1,9 @@
+package com.ood2.solid.worker;
+
+public interface Worker {
+	
+	int getEmployeeId();
+	String getEmployeeName();
+	double getSalary();
+
+}
