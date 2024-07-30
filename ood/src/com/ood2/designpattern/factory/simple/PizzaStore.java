9cdb5f2ebbe2839b1d1ee4235dd604921a255ee8
essteer <essteer@pm.me>
2024-07-30 11:13:20 +0800

    refactor: Move design pattern and hangman dirs under OOD dir

---
+++ ood/src/com/ood2/designpattern/factory/simple/PizzaStore.java
@@ -0,0 +1,23 @@
+package com.ood2.designpattern.factory.simple;
+
+public class PizzaStore {
+	SimplePizzaFactory factory;
+ 
+	public PizzaStore(SimplePizzaFactory factory) { 
+		this.factory = factory;
+	}
+ 
+	public Pizza orderPizza(String type) {
+		Pizza pizza = null;
+ 
+		pizza = factory.createPizza(type);
+ 
+		pizza.prepare();
+		pizza.bake();
+		pizza.cut();
+		pizza.box();
+
+		return pizza;
+	}
+
+}
