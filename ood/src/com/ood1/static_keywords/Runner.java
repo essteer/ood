ec94cac5ac5a276a3735dd5fa27962f91eac1673
essteer <essteer@pm.me>
2024-08-02 18:53:07 +0800

    feat: Add static example

--- ood/src/com/ood1/static_keywords/Runner.java
+++ ood/src/com/ood1/static_keywords/Runner.java
@@ -14,5 +14,11 @@ public class Runner {
 		System.out.println(matter * Math.pow(299792458, 2));
 		System.out.println("Energy to matter: " + energyToMatter);
 		System.out.println(energy / Math.pow(299792458, 2));
+		
+		final String[] arr = {"Apr", "May", "Jun", "Jul"};
+		arr[3] = "Julie";
+		for (String s : arr) {
+			System.out.println(s);
+		}
 	}
 }
